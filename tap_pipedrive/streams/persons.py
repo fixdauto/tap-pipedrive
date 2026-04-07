9dd499d756b9bc935420ab5d2d2e64d1e2d40c52
Nick Chang <nickchang@Nicks-Laptop-2.local>
2026-04-07 14:49:55 -0400

    fix: serialize persons phones/im/emails as JSON strings to prevent jsonb type mismatch

--- tap_pipedrive/streams/persons.py
+++ tap_pipedrive/streams/persons.py
@@ -1,3 +1,4 @@
+import json
 from tap_pipedrive.stream import DynamicSchemaStream
 
 class PersonsStream(DynamicSchemaStream):
@@ -14,7 +15,15 @@ class PersonsStream(DynamicSchemaStream):
         'last_outgoing_mail_time',
     )
 
+    ARRAY_FIELDS = ('phones', 'im', 'emails')
+
     def update_request_params(self, params):
         params = super().update_request_params(params)
         params['include_fields'] = ','.join(self.INCLUDE_FIELDS)
         return params
+
+    def process_row(self, row):
+        for field in self.ARRAY_FIELDS:
+            if field in row and isinstance(row[field], list):
+                row[field] = json.dumps(row[field])
+        return row
